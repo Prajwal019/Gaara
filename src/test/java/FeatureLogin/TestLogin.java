5c6b511c39b78bc0d3e052d480a0e53132e01d2b
Prajwal019 <prajwal.pr@aspireapp.com>
2024-06-01 14:24:35 +0530

    developed Framework

---
+++ src/test/java/FeatureLogin/TestLogin.java
@@ -0,0 +1,12 @@
+package FeatureLogin;
+
+import org.testng.annotations.Test;
+
+public class TestLogin
+{
+    @Test
+    public void testLoginCred()
+    {
+        System.out.println("Hi");
+    }
+}
